deb398587c5650a91593ad126d5a4d8816442abe
Tri Nguyen <mail@trimn.net>
2015-11-22 12:35:22 +0100

    Add header icons

--- views/header.js
+++ views/header.js
@@ -6,10 +6,10 @@ module.exports = () => {
       <div className="mainHeader-content">
         <img src="/img/tmn.jpg" alt="Tri Nguyen" className="profilePicture" />
         <h1 className="mainHeader-title">Tri Nguyen</h1>
-        <p>Mr. prokrastinering av 'Åh! Se der!'-borg</p>
+        <p >Gullet ska hjæm t Trondheim! <a className="gold" href="https://twitter.com/hashtag/RBK?src=hash">#RBK</a></p>
         <ul className="profileLocation-list">
-          <li><a href="https://github.com/tmn/">GitHub</a></li>
-          <li><a href="https://twitter.com/itmn/">Twitter</a></li>
+          <li><a href="https://github.com/tmn/"><i className="fa fa-github fa-custom"></i></a></li>
+          <li><a href="https://twitter.com/itmn/"><i className="fa fa-twitter fa-custom"></i></a></li>
           <li><a href="/is/professional">CV</a></li>
         </ul>
       </div>

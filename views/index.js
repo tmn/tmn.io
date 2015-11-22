570dda09b3de6c6e77a8bdcc79c1490386280583
Tri Nguyen <mail@trimn.net>
2015-11-22 12:35:29 +0100

    Add font awesome

--- views/index.js
+++ views/index.js
@@ -13,6 +13,7 @@ var Index = React.createClass({
         <link rel="stylesheet" type="text/css" href="/css/reset.css" />
         <link rel="stylesheet" type="text/css" href="/css/main.css" />
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.9.1/styles/github-gist.min.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
 
         <meta name="viewport" content="width=device-width" />
       </head>

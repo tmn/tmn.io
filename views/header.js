aeea0cdbae15aec9b329089c4e117aede81aa902
Tri Nguyen <mail@trimn.net>
2015-11-22 12:38:14 +0100

    Update header text

--- views/header.js
+++ views/header.js
@@ -6,7 +6,7 @@ module.exports = () => {
       <div className="mainHeader-content">
         <img src="/img/tmn.jpg" alt="Tri Nguyen" className="profilePicture" />
         <h1 className="mainHeader-title">Tri Nguyen</h1>
-        <p >Gullet ska hjæm t Trondheim! <a className="gold" href="https://twitter.com/hashtag/RBK?src=hash">#RBK</a></p>
+        <p >Gullet ska hjæm t Trondhjæm! <a className="gold" href="https://twitter.com/hashtag/RBK?src=hash">#RBK</a></p>
         <ul className="profileLocation-list">
           <li><a href="https://github.com/tmn/"><i className="fa fa-github fa-custom"></i></a></li>
           <li><a href="https://twitter.com/itmn/"><i className="fa fa-twitter fa-custom"></i></a></li>
